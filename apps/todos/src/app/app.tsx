921e5347be6fc76d2adfade2624d1ab9cc07ace0
odedmo <odedmoran222@gmail.com>
2022-04-21 23:44:06 +0300

    handle tests

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,14 +1,43 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
+import { useState } from 'react';
+// import NxWelcome from './nx-welcome';
+
+interface Todo {
+  title: string;
+}
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'Todo 1' },
+    { title: 'Todo 2' },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: `New todo ${Math.floor(Math.random() * 1000)}`,
+      },
+    ]);
+  }
+
   return (
     <StyledApp>
-      <NxWelcome title="todos" />
+      {/* <NxWelcome title="todos" /> */}
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'}>{t.title}</li>
+        ))}
+      </ul>
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
     </StyledApp>
   );
 }
