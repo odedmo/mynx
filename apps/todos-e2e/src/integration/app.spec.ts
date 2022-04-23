58cb66d58b50c5bbdee770b4e0ba3e60cce3aac2
odedmo <odedmoran222@gmail.com>
2022-04-23 22:09:44 +0300

    remove default test

--- apps/todos-e2e/src/integration/app.spec.ts
+++ apps/todos-e2e/src/integration/app.spec.ts
@@ -1,30 +1,11 @@
-import { getGreeting, getAddTodoButton, getTodos } from '../support/app.po';
-
-// describe('todos', () => {
-//   beforeEach(() => cy.visit('/'));
-
-//   it('should display welcome message', () => {
-//     // Custom command example, see `../support/commands.ts` file
-//     cy.login('my-email@something.com', 'myPassword');
-
-//     // Function helper example, see `../support/app.po.ts` file
-//     getGreeting().contains('Welcome todos');
-//   });
-// });
+import { getAddTodoButton, getTodos } from '../support/app.po';
 
 describe('TodoApps', () => {
-
   beforeEach(() => cy.visit('/'));
 
-
   it('should display todos', () => {
-
     getTodos().should((t) => expect(t.length).equal(2));
-
     getAddTodoButton().click();
-
     getTodos().should((t) => expect(t.length).equal(3));
-
   });
-
 });
