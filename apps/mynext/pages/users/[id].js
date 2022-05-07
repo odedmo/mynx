f71cc02fbe1cd7deea7140075ee9a51b256df00a
odedmo <odedmoran222@gmail.com>
2022-05-07 22:38:52 +0300

    add dynamic route

---
+++ apps/mynext/pages/users/[id].js
@@ -0,0 +1,43 @@
+export default function User({userData}) {
+  return (
+    <div>
+      {userData.name}
+      <br />
+      {userData.id}
+      <br />
+      {userData.email}
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+
+  const paths = [
+    {
+      params: {
+        id: '1'
+      },
+    },
+    {
+      params: {
+        id: '2'
+      },
+    }
+  ]
+
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  const userData = users.filter(user => user.id == params.id)[0]
+  return {
+    props: {
+      userData
+    }
+  };
+}
