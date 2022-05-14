0705ab05258f6f2a9c4e801c99ea5003f26cfa0e
odedmo <odedmoran222@gmail.com>
2022-05-14 21:50:31 +0300

    handle duplicated code

--- apps/mynext/utils/users.js
+++ apps/mynext/utils/users.js
@@ -1,13 +1,16 @@
-export async function getUserData(userId) {
+const getUsers = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users")
   const users = await res.json()
+  return users
+}
+
+export async function getUserData(userId) {
+  const users = await getUsers()
   return users.filter(user => user.id == userId)[0]
 }
 
 export async function getAllUsersIds() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await res.json()
-  
+  const users = await getUsers()
   return users.map((user) => {
     return {
       params: {
